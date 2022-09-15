a3c2420b47cea247c1496f1eb60e89d289d585ff
Selast Lambou <selastlambou@gmail.com>
2022-09-15 12:12:11 +0200

    ci

--- src/ui/components/table-cell.tsx
+++ src/ui/components/table-cell.tsx
@@ -26,4 +26,4 @@ export interface StyledTablePaginationProps extends TablePaginationBaseProps {
   component: any;
 }
 
-export const StyledTablePagination = styled(TablePagination)();
+export const StyledTablePagination = styled(TablePagination)<StyledTablePaginationProps>();
